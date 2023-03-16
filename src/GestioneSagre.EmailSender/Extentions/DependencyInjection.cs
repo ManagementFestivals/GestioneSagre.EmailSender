770f192a928e5df6b722a3009c5d46f6550edb3e
Angelo Pirola <angelo9871@gmail.com>
2023-03-17 00:01:34 +0100

    Rinominata classe statica

--- src/GestioneSagre.EmailSender/Extentions/DependencyInjection.cs
+++ src/GestioneSagre.EmailSender/Extentions/DependencyInjection.cs
@@ -1,14 +1,9 @@
-﻿using GestioneSagre.Logger.Extensions;
-
-namespace GestioneSagre.EmailSender.Extentions;
+﻿namespace GestioneSagre.EmailSender.Extentions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddEmailSenderService(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddMailKitEmailSenderService(this IServiceCollection services, IConfiguration configuration)
     {
-        services
-            .AddTransient<ILoggerService, LoggerService>();
-
         services
             .AddSingleton<IEmailSender, MailKitEmailSender>()
             .AddSingleton<IEmailClient, MailKitEmailSender>();
